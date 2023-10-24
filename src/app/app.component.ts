b8277bf52bc553357469b4997914f094b4a920eb
Pawan Sharma <pawankv89@gmail.com>
2023-10-24 18:57:55 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,21 +12,12 @@ export class AppComponent {
   translocoService: TranslocoService;
   constructor(translocoService: TranslocoService) {
     this.translocoService = translocoService;
-    // this language will be used as a fallback when a translation isn't found in the current language
-    //translate.setDefaultLang('fr');
+  }
 
-     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    //translate.use('fr');
-
-
-  //   translocoService.get('TITLE').subscribe((res: string) => {
-  //     console.log('TITLE ', res);
-  //     //=> 'hello world'
-  // });
-}
-
-useLanguage(language: string): void {
-  this.translocoService.setActiveLang(language);
-  //document.location.reload();
-}
+  useLanguage(language: string): void {
+    this.translocoService.setActiveLang(language);
+    //document.location.reload();
+    const title = this.translocoService.translate('TITLE');
+    console.log('TITLE ', title);
+  }
 }
